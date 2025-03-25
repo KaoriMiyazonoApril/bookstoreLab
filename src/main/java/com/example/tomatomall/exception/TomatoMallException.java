ded0f61558ee7fad24594ebf27e5424edfebf5aa
Alvin <2948703439@qq.com>
2025-03-25 15:44:39 +0800

    发生了什么

--- src/main/java/com/example/tomatomall/exception/TomatoMallException.java
+++ src/main/java/com/example/tomatomall/exception/TomatoMallException.java
@@ -14,19 +14,19 @@ public class TomatoMallException extends RuntimeException {
         return new TomatoMallException("格式错误");
     }
 
-    public static  TomatoMallException DuplicateName(){
+    public static TomatoMallException DuplicateName(){
         return new TomatoMallException("用户名重复");
     }
 
-    public static  TomatoMallException ProductNotFound(){
+    public static TomatoMallException ProductNotFound(){
         return new TomatoMallException("未找到对应商品");
     }
 
-    public static  TomatoMallException DuplicateProduct(){
+    public static TomatoMallException DuplicateProduct(){
         return new TomatoMallException("已存在同名商品");
     }
 
-    public static  TomatoMallException NoEnoughArguments(){
+    public static TomatoMallException NoEnoughArguments(){
         return new TomatoMallException("提供的参数数量不足");
     }
 }
